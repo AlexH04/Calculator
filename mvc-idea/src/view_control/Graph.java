6e0c78cc5a361bdd6471fa63423d6deb8ad9591d
Alex Hu <56702257+AlexH04@users.noreply.github.com>
2020-10-16 08:16:07 -0700

    Update Graph.java

--- mvc-idea/src/view_control/Graph.java
+++ mvc-idea/src/view_control/Graph.java
@@ -34,6 +34,9 @@ public class Graph extends JFrame {
 
     JLabel prompt = new JLabel("Please enter the a value:");
     JTextField inputBox = new JTextField(10);
+    JTextField inputBox2 = new JTextField(10);
+    JTextField inputBox3 = new JTextField(10);
+    JTextField inputBox4 = new JTextField(10);
     public static String a = null;
     public static String b = null;
     public static String c = null;
@@ -41,6 +44,9 @@ public class Graph extends JFrame {
 
     public Graph () { //constructor
         inputBox.setBounds(50, 140, 300, 40);
+        inputBox2.setBounds(50, 140, 300, 40);
+        inputBox3.setBounds(50, 140, 300, 40);
+        inputBox4.setBounds(50, 140, 300, 40);
         inputBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -68,14 +74,14 @@ public class Graph extends JFrame {
     private void promptB() {
         getContentPane().removeAll(); //removes previous prompt
         repaint(); //leaves background color intact
-        inputBox.setText(""); //resets the text box
+        inputBox2.setText(""); //resets the text box
         prompt.setText("Please enter the b value:"); //changes the prompt
         getContentPane().add(prompt);
-        getContentPane().add(inputBox);
-        inputBox.addActionListener(new ActionListener() {
+        getContentPane().add(inputBox2);
+        inputBox2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                b = inputBox.getText();
+                b = inputBox2.getText();
                 promptC(); //prompts for third input
             }
         });
@@ -84,14 +90,16 @@ public class Graph extends JFrame {
     private void promptC() {
         getContentPane().removeAll();
         repaint();
-        inputBox.setText("");
+        inputBox3.setText("");
         prompt.setText("Please enter the c value:");
-        getContentPane().add(prompt); //this still displays b for some reason
-        getContentPane().add(inputBox);
-        inputBox.addActionListener(new ActionListener() {
+        System.out.println("c test 1");
+        getContentPane().add(prompt);
+        getContentPane().add(inputBox3);
+        inputBox3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                c = inputBox.getText();
+                c = inputBox3.getText();
+                System.out.println(c);
                 promptD(); //prompts for third input
             }
         });
@@ -100,14 +108,14 @@ public class Graph extends JFrame {
     private void promptD() {
         getContentPane().removeAll();
         repaint();
-        inputBox.setText("");
+        inputBox4.setText("");
         prompt.setText("Please enter the d value:");
-        getContentPane().add(prompt); //this still displays b for some reason
-        getContentPane().add(inputBox);
-        inputBox.addActionListener(new ActionListener() {
+        getContentPane().add(prompt);
+        getContentPane().add(inputBox4);
+        inputBox4.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                d = inputBox.getText();
+                d = inputBox4.getText();
                 graph(a, b, c, d);
             }
         });
